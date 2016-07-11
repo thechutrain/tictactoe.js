935920698c88ba2e43244def49872a2c7c89b2c8
Alan Chu <chu.chuyalan@gmail.com>
2016-07-10 21:17:57 -0400

    working tic tac toe with jquery

---
+++ js/application.js
@@ -0,0 +1,163 @@
+$(document).ready(function(){
+	// event listener for the new_button clicked!
+	$("#new_game").on("click", newGame);
+
+	// event listener for when any of the boxes are clicked!
+	$(".box").on("click", changeBackground);
+
+	// $(".header").on("click", gameOver);
+
+})
+
+//////// Functions for the event listeners! //////// 
+	function newGame(){
+	// This Function will clear all the classes on the boxes
+	$(".box").removeClass("x o");
+	};
+
+	function checkCombinations(point1, point2, point3){
+		// var to contain winner, if no winner -1 is returned
+		var winner;
+		if (point1 == point2 && point1 == point3 && point1!=""){
+			winner = point1;
+		} else{
+			winner = "-1";
+		}
+		return winner;
+	};
+
+
+
+
+	// function isGameOver(){
+	// 	//  this function checks if the game is over
+	// 	// 1.) Get all the data points in the board
+	// 	var gameBoard = new Array(9);
+	// 	var centerBoard = $('centerBoard');
+	// 	for (var j=0; j< gameBoard.length; j++){
+	// 		// var mark = centerBoard.find('box' + j.toString()).data("mark");
+	// 		var box = $(centerBoard).find("box" + j.toString());
+	// 		alert(box);
+	// 		if (box.hasClass("x")){
+	// 			gameBoard[j] = "x";
+	// 		}
+	// 		else if (box.hasClass("o")){
+	// 			gameBoard[j] = "o";
+	// 		} else {
+	// 			gameBoard[j] = " ";
+	// 			// alert($(this));
+	// 		}
+	// 	};
+
+	// 	alert(gameBoard);
+
+	// 	// winning combinations
+	// 	var winningCombinations =[
+	// 	["0", "1", "2"],
+	// 	["3", "4", "5"],
+	// 	["6", "7", "8"],
+	// 	["0", "3", "6"],
+	// 	["1", "4", "7"],
+	// 	["2", "5", "8"],
+	// 	["0", "4", "8"],
+	// 	["2", "4", "6"]
+	// 	];
+
+	// 	for(var i=0; i < winningCombinations.length; i++){
+	// 		// alert(winningCombinations[i]);
+	// 		var pt1 = gameBoard[winningCombinations[i][0]];
+	// 		var pt2 = gameBoard[winningCombinations[i][1]];
+	// 		var pt3 = gameBoard[winningCombinations[i][2]];
+	// 		var results = checkCombinations(pt1, pt2, pt3);
+	// 		if (results == "x"){
+	// 			alert("GAME OVER: x won!");
+	// 			$(".box").off("click", changeBackground);
+	// 		} else if (results == "o"){
+	// 			alert("GAME OVER: o won!");
+	// 			$(".box").off("click", changeBackground);
+	// 		}
+	// 	}
+
+
+	// }
+
+	function changeBackground(){
+		var proceed = true;
+		// First check to see if box already has a class
+		if ($(this).hasClass("x") || $(this).hasClass("o")){
+				alert("That box has been selected already!");
+				proceed = false;
+		}
+
+		if (proceed == true){
+			// find out whose turn it is:
+			var whoseTurn = $("#model").find("#whoseTurn").data("turn");
+			// alert(whoseTurn);
+			if (whoseTurn == 0){
+				$(this).addClass('x');
+				$(this).data("mark", "x");
+				whoseTurn = "1";
+			} else {
+				$(this).addClass('o');
+				$(this).data("mark", "o");
+				whoseTurn = "0";
+			}
+			// change the value of whoseTurn
+			$("#model").find("#whoseTurn").data("turn", whoseTurn);
+
+			}
+
+		// Check for winner:
+		// 1.) create an array that represents board status
+		var gameBoard = new Array(9);
+		for (var j=0; j< gameBoard.length; j++){
+			var box_number = "#box" + j.toString();
+			var box = $(box_number);
+			if (box.hasClass("x")){
+				// alert("x");
+				gameBoard[j] = "x";
+			} else if (box.hasClass("o")){
+				// alert("o");
+				gameBoard[j] = "o";
+			} else {
+				// alert("No class");
+				gameBoard[j] = " ";
+			}
+		};
+
+		// 2.) check for any winning combinations
+	 	// winning combinations
+		var winningCombinations =[
+		["0", "1", "2"],
+		["3", "4", "5"],
+		["6", "7", "8"],
+		["0", "3", "6"],
+		["1", "4", "7"],
+		["2", "5", "8"],
+		["0", "4", "8"],
+		["2", "4", "6"]
+		];
+
+		var winner;
+		for (var i=0; i < winningCombinations.length; i++){
+			var pt1 = gameBoard[winningCombinations[i][0]];
+			var pt2 = gameBoard[winningCombinations[i][1]];
+			var pt3 = gameBoard[winningCombinations[i][2]];
+			// alert("Here are the three points: " + pt1 + ", " + pt2 + ", " + pt3);
+			var result = checkCombinations(pt1, pt2, pt3);
+			if (result == "x"){
+				winner = "x";
+			} else if (result == 'o'){
+				winner = 'o';
+			} 
+		};
+
+		if (winner != undefined){
+			alert("The winner is: " + winner);
+		}
+
+
+		};
+
+
+	
