deabb8b065a138483009c36e4f5d9d31907a5839
Alan Chu <chu.chuyalan@gmail.com>
2016-07-17 15:53:16 -0400

    added x and o image to selected box and view clear_x_o function works wonderfully

--- js/application_version2.js
+++ js/application_version2.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
 	// TESTING
 	view.display_x('box0');
 	view.display_o('box1');
-	view.clear_x_o();
+	// view.clear_x_o();
 	view.displayMessage("tap tap tap, is this thing working?");
 	// alert(model.winningCombinations);
 
@@ -46,6 +46,10 @@ var view = {
 	},
 
 	clear_x_o: function(){
+		// first remove the inner element that hosts png image
+		$(".x").children().remove();
+		$(".o").children().remove();
+		// remove the outer box's class of x or o
 		$(".box").removeClass("x o");
 	},
 
@@ -53,10 +57,14 @@ var view = {
 
 //////// Model //////// 
 var model = {
-	// Properties
+	// MODEL - Properties
 	gameNumber: 0,
 	whoseTurn: 0,
-	gameBoard: new Array(9),
+	// gameBoard: new Array(9),
+	gameBoard: [
+	'x','x','o',
+	'', '', '',
+	'', '', ''],
 	winningCombinations: [
 		["0", "1", "2"],
 		["3", "4", "5"],
@@ -69,117 +77,61 @@ var model = {
 		],
 	winner: undefined,
 
-	// Methods
+	// MODEL - Methods
 	newGame: function(){
 		// clear the x & o classes
 		view.clear_x_o();
 		// reset variables
-		this.winner = undefined;
-		this.gameBoard = new Array(9);
-		this.whoseTurn = 0;
+		model.winner = undefined;
+		model.gameBoard = new Array(9);
+		model.whoseTurn = 1;
 		// display to the user, that theres been a new game
 		view.displayMessage("You just started a new game!");
-	}
-}
+	},
 
-//////// Controller //////// 
-var controller = {
-	
-}
-
-
-//////// Functions for the event listeners! //////// 
-	function newGame(){
-	// This Function will clear all the classes on the boxes
-	$(".box").removeClass("x o");
-	};
-
-	function checkCombinations(point1, point2, point3){
-		// var to contain winner, if no winner -1 is returned
-		var winner;
-		if (point1 == point2 && point1 == point3 && point1!=""){
-			winner = point1;
-		} else{
-			winner = "-1";
-		}
-		return winner;
-	};
-
-	function changeBackground(){
-		var proceed = true;
-		// First check to see if box already has a class
-		if ($(this).hasClass("x") || $(this).hasClass("o")){
-				alert("That box has been selected already!");
-				proceed = false;
-		}
-
-		if (proceed == true){
-			// find out whose turn it is:
-			var whoseTurn = $("#model").find("#whoseTurn").data("turn");
-			// alert(whoseTurn);
-			if (whoseTurn == 0){
-				$(this).addClass('x');
-				$(this).data("mark", "x");
-				whoseTurn = "1";
-			} else {
-				$(this).addClass('o');
-				$(this).data("mark", "o");
-				whoseTurn = "0";
+	// this function checks if the game has a winner
+	gameWinner: function(){
+		// Use a function to check if game has a winning combination
+		var checkCombinations = function(point1, point2, point3) {
+			// var to contain winner, if no winner -1 is returned
+			var winner;
+			if (point1 == point2 && point1 == point3 && point1!="" && point1!=undefined){
+				winner = point1;
+			} else{
+				winner = "-1";
 			}
-			// change the value of whoseTurn
-			$("#model").find("#whoseTurn").data("turn", whoseTurn);
-
-			}
-
-		// Check for winner:
-		// 1.) create an array that represents board status
-		var gameBoard = new Array(9);
-		for (var j=0; j< gameBoard.length; j++){
-			var box_number = "#box" + j.toString();
-			var box = $(box_number);
-			if (box.hasClass("x")){
-				// alert("x");
-				gameBoard[j] = "x";
-			} else if (box.hasClass("o")){
-				// alert("o");
-				gameBoard[j] = "o";
-			} else {
-				// alert("No class");
-				gameBoard[j] = " ";
-			}
+			return winner;
 		};
 
-		// 2.) check for any winning combinations
-	 	// winning combinations
-		var winningCombinations =[
-		["0", "1", "2"],
-		["3", "4", "5"],
-		["6", "7", "8"],
-		["0", "3", "6"],
-		["1", "4", "7"],
-		["2", "5", "8"],
-		["0", "4", "8"],
-		["2", "4", "6"]
-		];
-
+		// loop through all the winning combinations to see if winner
 		var winner;
-		for (var i=0; i < winningCombinations.length; i++){
-			var pt1 = gameBoard[winningCombinations[i][0]];
-			var pt2 = gameBoard[winningCombinations[i][1]];
-			var pt3 = gameBoard[winningCombinations[i][2]];
+		for (var i=0; i < model.winningCombinations.length; i++){
+			var pt1 = model.gameBoard[model.winningCombinations[i][0]];
+			var pt2 = model.gameBoard[model.winningCombinations[i][1]];
+			var pt3 = model.gameBoard[model.winningCombinations[i][2]];
 			// alert("Here are the three points: " + pt1 + ", " + pt2 + ", " + pt3);
 			var result = checkCombinations(pt1, pt2, pt3);
 			if (result == "x"){
 				winner = "x";
 			} else if (result == 'o'){
 				winner = 'o';
-			} 
+			} else{
+				winner = -1;
+			}
 		};
+		model.winner = winner;
+		return winner;
+	},
 
-		if (winner != undefined){
-			alert("The winner is: " + winner);
-		}
+	// next function here
+}
 
+//////// Controller //////// 
+var controller = {
+	//CONTROLLER - properties
 
-		};
+	// CONTROLLER - methods
+	
+}
+
 
